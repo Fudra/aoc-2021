be0c89a4cc2a0e4dd5fab392433437c47f740485
Philipp Wahle <info@wahlemedia.de>
2021-12-17 12:16:37 +0100

    add template

---
+++ 00_template/index.js
@@ -0,0 +1,20 @@
+const utils = require('../utils');
+
+const step01 = () => {
+  const data = utils.openFile(utils.type.FILE_SAMPLE);
+
+  let result = '';
+
+  console.log(`result => ${result}`);
+};
+
+const step02 = () => {
+  const data = utils.openFile(utils.type.FILE_SAMPLE);
+
+  let result = '';
+
+  console.log(`result => ${result}`);
+};
+
+step01();
+//step02();
